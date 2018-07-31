71a54d677825727c1e02ba4e376afc3b535512a2
Hanningz <xiongqionghai@gmail.com>
2018-07-31 21:32:27 +0300

    Update mainIn.js

--- js/mainIn.js
+++ js/mainIn.js
@@ -225,7 +225,7 @@ var $gtMap = [[
                                         tempBody[i+1] = tempTR;
                                     }
 
-                                    var uciSystem = uciSystemDes + "<br><div style='margin-top: 20px; font-weight: bold; font-size: 14px;'>" + uciSystemDesOne + "</span><br></div><br>" +  tempBody.join('');
+                                    var uciSystem = uciSystemDes + "<br><div style='margin-top: 20px; font-weight: bold; font-size: 14px;'>" + uciSystemDesOne + "<br></div><br>" +  tempBody.join('');
                                     json1["uciChildTwoValue"] = uciSystem;
 
                                     // Timeserver
@@ -257,7 +257,7 @@ var $gtMap = [[
                                         tempBodyuciTSTwo[i+1] = tempTR;
                                     }
 
-                                    var uciTimeServer = uciTimeServerDes + "<br><div style='margin-top: 20px; font-weight: bold; font-size: 14px;'>" + uciTimeServerOne + "</div><br>" +  tempBodyuciTSOne.join('') + "<div style='margin-top: 30px; font-size: 14px; font-weight: bold;'>" + uciTimeServerTwo + "</div>" + tempBodyuciTSTwo.join('') ;
+                                    var uciTimeServer = uciTimeServerDes + "<br><div style='margin-top: 20px; font-weight: bold; font-size: 14px;'>" + uciTimeServerOne + "</div><br>" +  tempBodyuciTSOne.join('') + "<div style='margin-top: 30px; font-size: 14px; font-weight: bold;'>" + uciTimeServerTwo + "<br></div>" + tempBodyuciTSTwo.join('') ;
                                     json1["uciChildThreeValue"] = uciTimeServer;
 
                                 }
